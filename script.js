42d77cd092618fe0f53f2172e92aba15874be308
aninhadoryan <00001094760067sp@al.educacao.sp.gov.br>
2024-11-24 19:57:32 -0300

    subindo

---
+++ script.js
@@ -0,0 +1,51 @@
+// Canvas Animation
+const canvas = document.getElementById('headerCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+const numberOfParticles = 100;
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
+  }
+  draw() {
+    ctx.fillStyle = '#00e6e6';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init() {
+  particlesArray = [];
+  for (let i = 0; i < numberOfParticles; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particlesArray.forEach((particle, index) => {
+    particle.update();
+    particle.draw();
+    if (particle.size <= 0.3) {
+      particlesArray.splice(index, 1);
+    }
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
